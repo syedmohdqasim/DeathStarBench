29c4e3d670d807c980573665796202deab34bdd3
Mert TOSLALI <toslali@bu.edu>
2022-02-08 14:08:10 -0500

    Fix repeating variable name

--- socialNetwork/src/TextService/TextHandler.h
+++ socialNetwork/src/TextService/TextHandler.h
@@ -60,10 +60,10 @@ void TextHandler::ComposeText(
   // tsl: sleep
 
     std::ifstream fin("/astraea-spans/statesds");
-    std::string s;
+    std::string str;
 
-    while (getline(fin,s)) {
-        if (s.find("compose_text_server") != std::string::npos) {
+    while (getline(fin,str)) {
+        if (str.find("compose_text_server") != std::string::npos) {
             
             // sleep now
                 unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
