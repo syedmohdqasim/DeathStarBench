4d3470ddfbfeda4fbb18478ba03782f02a18e4ea
Mert Toslali <merttoslali@Merts-MacBook-Pro.local>
2022-03-15 11:48:48 -0400

    Change sleep location

--- socialNetwork/docker/thrift-microservice-deps/cpp/Tracer.cpp
+++ socialNetwork/docker/thrift-microservice-deps/cpp/Tracer.cpp
@@ -91,24 +91,7 @@ Tracer::StartSpanWithOptions(string_view operationName,
         //     }
         // }
         
-        // // tsl: sleep
-        // _logger->info("----Mertiko fixed sleep checking file");
-        // std::ifstream fin2("/astraea-spans/sleeps");
-        // std::string sleep;
 
-        // while (getline(fin2,sleep)) {
-        //     if (sleep.find(operationName) != std::string::npos) {
-                
-        //         unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-        //         std::default_random_engine generator(seed);
-                
-        //         std::normal_distribution<> d{100,30};
-        //         int x = std::round(d(generator));
-        //          _logger->info("+++Mertiko sleep microseconds");
-        //          _logger->info(std::to_string(x));
-        //         std::this_thread::sleep_for(std::chrono::microseconds(x));
-        //     }
-        // }
 
         const auto result = analyzeReferences(options.references);
         const auto* parent = result._parent;
@@ -179,6 +162,32 @@ Tracer::StartSpanWithOptions(string_view operationName,
         SteadyClock::time_point startTimeSteady;
         std::tie(startTimeSystem, startTimeSteady) =
             determineStartTimes(options);
+
+
+
+        // tsl: sleep
+        _logger->info("----Mertiko fixed sleep checking file");
+        std::ifstream fin2("/astraea-spans/sleeps");
+        std::string sleep;
+
+        while (getline(fin2,sleep)) {
+            if (sleep.find(operationName) != std::string::npos) {
+                
+                unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+                std::default_random_engine generator(seed);
+                
+                std::normal_distribution<> d{100,50};
+                int x = std::round(d(generator));
+                if ( x < 0 )
+                {
+                    x = 0;
+                }
+                 _logger->info("+++Mertiko sleep microseconds");
+                 _logger->info(std::to_string(x));
+                std::this_thread::sleep_for(std::chrono::microseconds(x));
+            }
+        }
+
         return startSpanInternal(ctx,
                                  operationName,
                                  startTimeSystem,
