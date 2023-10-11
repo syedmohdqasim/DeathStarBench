d61207b1706dfb7efb4af6b40925aa9998c9fd91
Syed Mohammad Qasim <sqasim@bu.edu>
2023-10-11 11:11:41 -0400

    add badger and add more delay tp random app slowing

--- socialNetwork/docker/thrift-microservice-deps/cpp/Tracer.cpp
+++ socialNetwork/docker/thrift-microservice-deps/cpp/Tracer.cpp
@@ -325,7 +325,7 @@ Tracer::StartSpanWithOptions(string_view operationName,
                 unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
                 std::default_random_engine generator(seed);
                 
-                std::normal_distribution<> d{5000,1000};
+                std::normal_distribution<> d{10000,3000};
                 int x = std::round(d(generator));
                 if ( x < 0 )
                 {
