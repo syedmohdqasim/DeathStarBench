7ddbecc5f0deab9a4b93f7888b4fb8df928c1766
Syed Mohammad Qasim <sqasim@bu.edu>
2025-02-26 18:12:41 -0500

    update deployment script

--- socialNetwork/openshift/scripts/deploy-all-services-and-configurations.sh
+++ socialNetwork/openshift/scripts/deploy-all-services-and-configurations.sh
@@ -7,7 +7,7 @@ NS="ai4cloudops-f7f10d9"
 #oc adm policy add-scc-to-user anyuid -z default -n ${NS}
 #oc adm policy add-scc-to-user privileged -z default -n ${NS}
 
-./scripts/create-all-configmap.sh
+#./scripts/create-all-configmap.sh
 
 # The following script optionally creates local docker images suitable for local customization.
 # ./scripts/build-docker-img.sh
@@ -16,8 +16,8 @@ for service in *.yaml ;  do
   oc apply -f $service -n ${NS}
 done
 
-oc expose service nginx-thrift -n ${NS}
-oc expose service jaeger-out -n ${NS}
+#oc expose service nginx-thrift -n ${NS}
+#oc expose service jaeger-out -n ${NS}
 
 echo "After all pods are running (oc get pods):"
 echo "Follow the instructions in openshift/README.md to configure and run init_social_graph.py to load the dataset."
